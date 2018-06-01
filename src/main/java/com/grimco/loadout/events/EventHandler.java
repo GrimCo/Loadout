3009b0b8e40f888426985d9609dabb34276b11f7
mallrat208 <marwat208@gmail.com>
2018-06-01 03:52:28 -0400

    Removed Capability as it was overkill
    It also was causing issues due to my poor implementation

--- src/main/java/com/grimco/loadout/events/EventHandler.java
+++ src/main/java/com/grimco/loadout/events/EventHandler.java
@@ -1,62 +1,76 @@
 package com.grimco.loadout.events;
 
+import com.grimco.loadout.Loadout;
+import com.grimco.loadout.Loadout.ModConfig;
 import com.grimco.loadout.LoadoutManager;
-import com.grimco.loadout.capability.CapabilityLoadout;
-import com.grimco.loadout.capability.ILoadout;
-import com.grimco.loadout.capability.LoadoutImp;
-import com.grimco.loadout.capability.LoadoutImp.Provider;
-import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemArmor;
 import net.minecraft.nbt.NBTTagCompound;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent;
-import net.minecraftforge.fml.relauncher.Side;
+
 import net.minecraftforge.items.ItemHandlerHelper;
 
-import java.util.Set;
-
-@EventBusSubscriber(Side.SERVER)
+@EventBusSubscriber()
 public class EventHandler
 {
-	
-	@SubscribeEvent
-	public static void onCapabilityAttach(AttachCapabilitiesEvent<Entity> event)
-	{
-		if(event.getObject() instanceof EntityPlayer)
-		{
-			event.addCapability(Provider.NAME, new LoadoutImp.Provider());
-		}
-	}
-	
-	@SubscribeEvent
-	public static void onPlayerClone(PlayerEvent.Clone event)
-	{
-		if(event.getEntity().hasCapability(CapabilityLoadout.LOADOUT, null))
-		{
-			ILoadout capOld = event.getOriginal().getCapability(CapabilityLoadout.LOADOUT, null);
-			ILoadout capNew = event.getEntity().getCapability(CapabilityLoadout.LOADOUT, null);
-			
-			capOld.getLoadouts().forEach(loadout -> capNew.setLoadout(loadout));
-		}
-	}
+	public static final String LOUDOUT_TAG = "LOADOUT_KEYS";
 	
 	@SubscribeEvent
 	public static void onPlayerJoinedWorld(PlayerLoggedInEvent event)
 	{
-		if(event.player.hasCapability(CapabilityLoadout.LOADOUT,null))
-		{
-			EntityPlayer player = event.player;
-			ILoadout loadoutCap = player.getCapability(CapabilityLoadout.LOADOUT, null);
-			LoadoutManager.loadouts.forEach((key,list) -> {
-				if(!loadoutCap.receivedLoadout(key))
-				{
-					list.forEach(stack -> ItemHandlerHelper.giveItemToPlayer(player, stack.copy()));
-					loadoutCap.setLoadout(key);
-				}
-			});
-		}
+		if(event.player.world.isRemote)
+			return;
+
+		EntityPlayer player = event.player;
+		
+		
+		NBTTagCompound playerData = player.getEntityData();
+		NBTTagCompound data = playerData.getCompoundTag(EntityPlayer.PERSISTED_NBT_TAG);
+		
+		NBTTagCompound keys = new NBTTagCompound();
+		
+		if(data.hasKey(LOUDOUT_TAG))
+			keys = data.getCompoundTag(LOUDOUT_TAG);
+		
+		NBTTagCompound finalKeys=keys;
+		LoadoutManager.loadouts.forEach((key,list) -> {
+			if(!finalKeys.hasKey(key))
+			{
+				list.forEach(stack -> {
+					if(ModConfig.equipArmor && stack.getItem()instanceof ItemArmor)
+					{
+						if(player.getItemStackFromSlot(EntityLiving.getSlotForItemStack(stack)).isEmpty())
+						{
+							player.setItemStackToSlot(EntityLiving.getSlotForItemStack(stack),stack.copy());
+						}
+						else
+						{
+							ItemHandlerHelper.giveItemToPlayer(player, stack.copy());
+						}
+					}
+					else
+					{
+						ItemHandlerHelper.giveItemToPlayer(player, stack.copy());
+					}
+				});
+				
+				finalKeys.setByte(key,(byte)1);
+			}
+		});
+		
+		NBTTagCompound tempKeyList = finalKeys;
+		
+		tempKeyList.getKeySet().forEach(tag -> {
+			if(!LoadoutManager.loadouts.keySet().contains(tag))
+				finalKeys.removeTag(tag);
+		});
+		
+		data.setTag(LOUDOUT_TAG,finalKeys);
+		playerData.setTag(EntityPlayer.PERSISTED_NBT_TAG, data);
+		
 	}
 }
+
